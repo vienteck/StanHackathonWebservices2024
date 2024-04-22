8f93a41cde67fd27c0dc4d22f8c23bd1c5cfcc68
Victor Naranjo <vanaranjo@tid.org>
2024-04-22 15:57:12 -0700

    working on presentation

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Basic GET endpoint which will send a JSON response
-app.get('/api/greeting', (req, res) => {
-  res.json({ message: 'Hello, world!' });
+app.get("/api/greeting", (req, res) => {
+  res.json({ message: "Hello, world!" });
 });
-
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-//app.use(() => {console.log('hello world')})
+app.use((req, res, next) => {
+  console.log(req.rawHeaders);
+  console.log('Time: %d', Date.now())
+  next()
+});
 
 // Define a route for a POST request
-app.post('/api/users', (req, res) => {
+app.post("/api/users", (req, res) => {
   const { name, email } = req.body;
   // Here you can handle the user data, such as saving it to a database
-  res.json({ message: 'User created successfully', name, email });
+  res.json({ message: "User created successfully", name, email });
 });
 
 // Start the server
